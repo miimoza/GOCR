a8eaa32180163f6057ff019990594a1f0850120f
Dorian Vinai <dorian.vinai@epita.fr>
2020-09-28 18:39:33 +0200

    started project

--- main.go
+++ main.go
@@ -1,13 +1,22 @@
-// You can edit this code!
-// Click here and start typing.
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "src/img_loader"
+)
 
 const (
    VERSION = "alpha (α)"
+   IMAGE_PATH = "res/test.jpg"
 )
 
 func main() {
 	fmt.Printf("Welcome to GOCR %s!\n", VERSION)
+
+    rgb_array := img_loader.getPixels(IMAGE_PATH)
+    fmt.Print(rgb_array)
+
+    //img.preprocess(pixel_array)
+
+    //img.generateChar(pixel_array)
 }
---
+++ src/img_loader.go
@@ -0,0 +1,56 @@
+package img_loader
+
+import (
+    "fmt"
+    "image"
+    "image/jpeg"
+    "os"
+    "io"
+)
+
+type RGB_Pixel struct {
+    R int
+    G int
+    B int
+}
+
+type RGB_Image [][]Pixel
+
+func getPixels(path) (RGB_Image) {
+    image.RegisterFormat("jpeg", "jpeg", jpeg.Decode, jpeg.DecodeConfig)
+
+    file, err := os.Open(path)
+    if err != nil {
+        fmt.Println("Error: File could not be opened")
+        os.Exit(1)
+    }
+    defer file.Close()
+
+    img, _, err := image.Decode(file)
+    if err != nil {
+        fmt.Println("Error: Image could not be decoded")
+        os.Exit(1)
+    }
+
+    bounds := img.Bounds()
+    width, height := bounds.Max.X, bounds.Max.Y
+
+    var pixels RGB_Image
+    for y := 0; y < height; y++ {
+        var row []Pixel
+        for x := 0; x < width; x++ {
+            row = append(row, rgbaToRgb(img.At(x, y).RGBA()))
+        }
+        pixels = append(pixels, row)
+    }
+
+    return pixels
+}
+
+func rgbaToRgb(r uint32, g uint32, b uint32, a uint32) Pixel {
+    return RGB_Pixel {
+        int(r / 257),
+        int(g / 257),
+        int(b / 257)
+    }
+}
