34d1c1c75f661986cb869dce84e5c7cc9ec250eb
Dorian Vinai <dorian.vinai@epita.fr>
2020-09-28 17:41:34 +0200

    Added main.go

---
+++ main.go
@@ -0,0 +1,13 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import "fmt"
+
+const (
+   VERSION = "alpha (α)"
+)
+
+func main() {
+	fmt.Printf("Welcome to GOCR %s!\n", VERSION)
+}
