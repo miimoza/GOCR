46a6925cd7184cacc432949d2cabd0320dcfbe0f
Dorian Vinai <dorian.vinai@epita.fr>
2020-10-23 22:31:23 +0200

    implemented greyscale grey scalling separatly

--- src/img/binarization.go
+++ src/img/binarization.go
@@ -11,15 +11,34 @@ type BW_Pixel struct {
     B bool
 }
 
+type GS_Pixel struct {
+    V int
+}
+
 type BW_Image [][]BW_Pixel
+
+type GS_Image [][]GS_Pixel
 
 func Binarize(rgb_image RGB_Image) (BW_Image) {
 
-    var bw_image BW_Image
+
+    var gs_image GS_Image
     for _, s := range rgb_image {
-        var row []BW_Pixel
+        var row []GS_Pixel
         for _, s2 := range s {
-            var luminance float64 = float64(s2.R) * R_LUMINANCE + float64(s2.G) * G_LUMINANCE + float64(s2.B) * B_LUMINANCE
+            var luminance int = int(float64(s2.R) * R_LUMINANCE + float64(s2.G) * G_LUMINANCE + float64(s2.B) * B_LUMINANCE)
+            row = append(row, GS_Pixel {luminance})
+        }
+        gs_image = append(gs_image, row)
+    }
+
+
+
+    var bw_image BW_Image
+    for i, s := range rgb_image {
+        var row []BW_Pixel
+        for j, _ := range s {
+            var luminance int = gs_image[i][j].V
             B := false
             if luminance > THRESHOLD {
                 B = true
