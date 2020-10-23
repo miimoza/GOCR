2910977fc962f41588416e3f71ba6629aa14f860
Antoine Coulon <antoine.coulon@epita.fr>
2020-10-23 21:38:14 +0200

    ok

--- src/img/segmentation.go
+++ src/img/segmentation.go
@@ -24,8 +24,15 @@ func SaveImage(img_array BW_Image, path string) (){
     img := image.NewRGBA(image.Rect(0, 0, 100, 100))
 
     for i := 0; i < 100; i++ {
-		img.Set(i, i, color.RGBA{4, 4, 4, 4})
-	}
+        for j := 0; i < 100; j++ {
+            greyscale := 0
+            if img_array[i][j].B {
+                greyscale = 255
+            }
+            pxl := color.RGBA{4, greyscale, greyscale, greyscale}
+            img.Set(i, j, pxl)
+        }
+    }
 
     f, _ := os.OpenFile(path, os.O_WRONLY|os.O_CREATE, 0600)
     defer f.Close()
