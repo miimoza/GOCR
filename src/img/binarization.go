b85d5113f027d30370e70bcbd285a7a9a0a927bf
Dorian Vinai <dorian.vinai@epita.fr>
2020-10-23 21:18:15 +0200

    binarized

--- src/img/binarization.go
+++ src/img/binarization.go
@@ -1,5 +1,12 @@
 package img
 
+const (
+   R_LUMINANCE = 0.2126
+   G_LUMINANCE = 0.7152
+   B_LUMINANCE = 0.0722
+   THRESHOLD = 0.5
+)
+
 import (
     "fmt"
 )
@@ -12,12 +19,18 @@ type BW_Image [][]BW_Pixel
 
 func Binarize(rgb_image RGB_Image) (BW_Image) {
 
-    var bw_image BW_Image;
-
+    var bw_image BW_Image
     for _, s := range rgb_image {
+        var row []BW_Pixel
         for _, s2 := range s {
-            fmt.Println(s2)
+            luminance := s2.R * R_LUMINANCE
+                       + s2.G * G_LUMINANCE
+                       + s2.B * B_LUMINANCE
+            row = append(row, {
+                (luminance > THRESHOLD) ? true : false;
+            })
         }
+        bw_image = append(bw_image, row)
     }
 
     return bw_image
