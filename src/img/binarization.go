a2876717a016df6e7f4e83285604af5dae12f288
Dorian Vinai <dorian.vinai@epita.fr>
2020-10-23 23:20:54 +0200

    Implemented BHT

--- src/img/binarization.go
+++ src/img/binarization.go
@@ -1,10 +1,13 @@
 package img
 
+import (
+    "fmt"
+)
+
 const (
    R_LUMINANCE = 0.2126
    G_LUMINANCE = 0.7152
    B_LUMINANCE = 0.0722
-   THRESHOLD = 127
 )
 
 type BW_Pixel struct {
@@ -34,7 +37,36 @@ func Binarize(rgb_image RGB_Image) (BW_Image) {
         gs_image = append(gs_image, row)
     }
 
+    i_m := 127
+    fmt.Println("yes")
 
+    /*
+    i_s := 0
+    i_e := 255
+    i_m := int((i_s + i_e) / 2.0) // center of the weighing scale I_m
+    w_l := get_weight(i_s, i_m + 1, histogram) // weight on the left W_l
+    w_r := get_weight(i_m + 1, i_e + 1, histogram) // weight on the right W_r
+    for (i_s <= i_e) {
+        if (w_r > w_l) { // right side is heavier
+            w_r -= histogram[i_e]
+            i_e--
+            if (((i_s + i_e) / 2) < i_m) {
+                w_r += histogram[i_m]
+                w_l -= histogram[i_m]
+                i_m--
+            }
+        } else if (w_l >= w_r) { // left side is heavier
+            w_l -= histogram[i_s]
+            i_s++
+            if (((i_s + i_e) / 2) >= i_m) {
+                w_l += histogram[i_m + 1]
+                w_r -= histogram[i_m + 1]
+                i_m++
+            }
+        }
+        fmt.Println(i_s, i_e, ",", i_m)
+    }
+    */
 
     var bw_image BW_Image
     for i, s := range rgb_image {
@@ -42,7 +74,7 @@ func Binarize(rgb_image RGB_Image) (BW_Image) {
         for j, _ := range s {
             var luminance int = gs_image[i][j].V
             B := false
-            if luminance > THRESHOLD {
+            if luminance > i_m {
                 B = true
             }
             row = append(row, BW_Pixel {B})
@@ -51,4 +83,12 @@ func Binarize(rgb_image RGB_Image) (BW_Image) {
     }
 
     return bw_image
+}
+
+func get_weight(start int, end int, histogram [256]int) (int) {
+    var weight int = 0
+    for i := start; i < end; i++ {
+        weight += histogram[i]
+    }
+    return weight
 }
